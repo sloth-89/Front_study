ffad56bf51bbadf285742002c2444f43e239b7b5
sloth-89 <restvoice2020@gmail.com>
2023-03-25 09:58:31 +0900

    230325

---
+++ array03_spread.js
@@ -0,0 +1,5 @@
+
+const color = ['red', 'orange', 'yellow']
+const nextColor = [,,,color,'green', 'blue']
+
+console.log(nextColor)
---
+++ math01.js
@@ -0,0 +1,11 @@
+
+const fruit = ["사과", "포도"]
+console.log(fruit)
+
+const squares1 = Array(3);
+const squares2 = Array(3).fill(null);
+const squares3 = Array(3).fill("데이터값");
+
+console.log(squares1) // [empty x 3]
+console.log(squares2) // [null, null, null]
+console.log(squares3) // ["데이터값", "데이터값", "데이터값"]
